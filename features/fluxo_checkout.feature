a1ca9ac22935776ed70b9b830582d93983a3e746
Jeferson Luis Monroy <jlmonroy24@gmail.com>
2020-07-20 18:05:29 -0300

    Code refatoring Part 1

--- features/fluxo_checkout.feature
+++ features/fluxo_checkout.feature
@@ -4,7 +4,7 @@ Funcionalidade: Realizar fluxo de compra até o checkout
 
   Cenario: Fluxo de compra
     Dado que eu acesso a minha conta
-    E realizo a busca do produto 'x' e adiciono ao produto o carrinho
+    E realizo a busca do produto 'backpack' e adiciono ao produto o carrinho
     E prossigo para o checkout e preencho o formulario de entrega
     Quando realizo o pagamento
     Então eu fecho o pedido com sucesso
--- features/step_definitions/fluxo_checkout_steps.rb
+++ features/step_definitions/fluxo_checkout_steps.rb
@@ -7,10 +7,10 @@ Dado('que eu acesso a minha conta') do
   assert_text('My Account', wait:10)
 end
 
-Dado('realizo a busca do produto {string} e adiciono ao produto o carrinho') do |string|
-  find(:xpath, "//input[@id='search']").set "backpack"
-  find(:xpath, "//input[@id='search']").send_keys :enter
+Dado('realizo a busca do produto {string} e adiciono ao produto o carrinho') do |produto|
+  find(:xpath, "//input[@id='search']").set(produto).send_keys :enter 
   first(:xpath, "//img[@class='product-image-photo']").click
+
   click_button "Add to Cart"
   expect(page).to have_content 'You added'
   find(:xpath, "//span[@class='counter qty']").click
@@ -21,14 +21,15 @@ Dado('realizo a busca do produto {string} e adiciono ao produto o carrinho') do 
 end
 
 Dado('prossigo para o checkout e preencho o formulario de entrega') do
-  find(:xpath, "//input[@name='street[0]']").set "Rua Jaime Taveira, 91"
-  find(:xpath, "//input[@name='street[1]']").set "Apto 23B"
-  find(:xpath, "//input[@name='street[2]']").set "Chacara Cruzeiro do Sul"
-  find(:xpath, "//input[@name='city']").set "São Paulo"
-  find(:xpath, "//input[@name='postcode']").set "03732-060"
+  find(:xpath, "//input[@name='street[0]']").set("Rua Jaime Taveira, 91")
+  find(:xpath, "//input[@name='street[1]']").set("Apto 23B")
+  find(:xpath, "//input[@name='street[2]']").set("Chacara Cruzeiro do Sul")
+  find(:xpath, "//input[@name='city']").set("São Paulo")
+  find(:xpath, "//input[@name='postcode']").set("03732-060")
   select "Brazil", :from => "country_id"
   select "São Paulo", :from => "region_id"
-  find(:xpath, "//input[@name='telephone']").set "(11) 976160727"
+  find(:xpath, "//input[@name='telephone']").set("(11) 976160727")
+  
   page.has_no_selector?(:xpath, "//div[@class='loading-mask']")
   find(:xpath, "//input[@value='flatrate_flatrate']").click
   click_button "Next"
